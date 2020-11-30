6f4409a546dc3a18e2a15b3b245b34b87021d204
Austin Noska <noska.austin@gmail.com>
2020-11-29 19:46:24 -0500

    Create controllable player with animations

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator playerAnimator;
+
+    private bool m_IsJumping;
+    public bool IsJumping
+    {
+        get { return m_IsJumping; }
+        set 
+        {
+            if (value == m_IsJumping)
+                return;
+
+            m_IsJumping = value;
+            playerAnimator.SetBool("IsJumping", m_IsJumping);
+
+            if (m_IsJumping)
+                StartCoroutine(Jumping());
+        }
+    }
+
+    private bool m_IsRunning;
+    public bool IsRunning
+    {
+        get { return m_IsRunning; }
+        set
+        {
+            if (value == m_IsRunning)
+                return;
+
+            m_IsRunning = value;
+            playerAnimator.SetBool("IsRunning", m_IsRunning);
+        }
+    }
+
+    private const string ANIMATION_IDLE_NAME = "Idle01";
+    private const string ANIMATION_JUMP_NAME = "Jump";
+    private const string ANIMATION_RUN_NAME = "Run";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsRunning && !IsJumping)
+            playerAnimator.Play(ANIMATION_IDLE_NAME);
+        else if (IsRunning && !IsJumping)
+            playerAnimator.Play(ANIMATION_RUN_NAME);
+    }
+
+    private IEnumerator Jumping()
+    {
+        playerAnimator.Play(ANIMATION_JUMP_NAME);
+        yield return new WaitForSeconds(1.1f);
+        IsJumping = false;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    private Vector3 playerVelocity;
+    public bool isGrounded;
+
+    private PlayerAnimation playerAnimation;
+
+    void Start()
+    {
+        // Hide and lock cursor on startup
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        playerAnimation = this.GetComponent<PlayerAnimation>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Unlock and show cursor if user holds esc
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        // Jumps
+
+        isGrounded = controller.isGrounded;
+        if (isGrounded && playerVelocity.y < 0)
+            playerVelocity.y = 0f;
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space) && !playerAnimation.IsJumping)
+        {
+            playerAnimation.IsJumping = true;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        // Movements
+        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            playerAnimation.IsRunning = true;
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            playerAnimation.IsRunning = false;
+        }
+    }
+}
