5e2e06ad169598f1d983a91a71581318dc29f360
Austin Noska <noska.austin@gmail.com>
2020-12-02 16:19:11 -0500

    Add UI timer

--- Assets/Scripts/MountainLevelManager.cs
+++ Assets/Scripts/MountainLevelManager.cs
@@ -1,24 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MountainLevelManager : MonoBehaviour
 {
+    // Player members
     public GameObject player;
-
     public Transform spawnPoint;
 
+    // UI
     public Text outOfBoundsText;
+    public Text timerText;
+
+    // Timer info
+    public float startTime;
+    public float timeLimit;
 
     // Start is called before the first frame update
     void Start()
     {
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateTimerText();
+    }
+
+    private void UpdateTimerText()
+    {
+        float timeRemaining = timeLimit - (Time.time - startTime);
+        timerText.text = string.Format("Time Remaining:\n{0}", TimeSpan.FromSeconds(timeRemaining).ToString(@"mm\:ss"));
     }
 
     public void OnPlayerOutOfBounds()
