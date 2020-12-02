15984cbcab74e9928a83d80dd90fad12e7c17a08
Austin Noska <noska.austin@gmail.com>
2020-12-01 19:33:15 -0500

    Add out of bounds detection

---
+++ Assets/Scripts/MountainLevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MountainLevelManager : MonoBehaviour
+{
+    public GameObject player;
+    private CharacterController playerController;
+
+    public Transform spawnPoint;
+
+    public Text outOfBoundsText;
+
+    void Awake()
+    {
+        if (player != null)
+            playerController = player.GetComponent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnPlayerOutOfBounds()
+    {
+        player.SetActive(false);
+        player.transform.position = spawnPoint.transform.position;
+        player.SetActive(true);
+
+        if (!outOfBoundsText.gameObject.activeSelf)
+            StartCoroutine(UiOnPlayerOutOfBounds());
+    }
+
+    private IEnumerator UiOnPlayerOutOfBounds()
+    {
+        outOfBoundsText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        outOfBoundsText.gameObject.SetActive(false);
+    }
+}
