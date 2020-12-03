1f73bcbf40b1eefdf94ca365a22ddda0c6ced64a
Nathan Midgett <natemidgett@gmail.com>
2020-12-03 16:59:37 -0500

    Finished Pyramid Challenge, Started UI
    Made a slab that goes up and down like an elevator. Started UI that tells player what to do and how many coins they have.

---
+++ Assets/Scripts/UpDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+
+    
+        public GameObject slab;
+        public float speed;
+
+        public void Update()
+        {
+            float y = Mathf.PingPong(Time.time * speed, 2) * 6 - 3 - 83;
+            slab.transform.position = new Vector3((float)-1.14, y, (float)187.37);
+        }
+    
+}
