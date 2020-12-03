e143bbc0bd79e71a183372c9af35dd8301cab880
Nathan Midgett <natemidgett@gmail.com>
2020-12-03 18:50:20 -0500

    Partially done Red Coins
    The player can now collect red coins!! All I have to do now is make sure that the UI displays the number of coins that have been collected!!

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CoinCounter : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public Text uitCoins; //UI Text CoinCounterText
+    //[Header("Set Dynamically")]
+    
+    
+    static public int CoinsCollected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateGUI()
+    {
+        // Show the data in the GUITexts
+        //uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+        uitCoins.text = "Coins Collected: " + CoinsCollected;
+    }
+    void Update()
+    {
+        UpdateGUI();
+    }
+}
---
+++ Assets/Scripts/RCoin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RCoin : MonoBehaviour
+{
+    public static int collected = 0;
+    [Header("Set Dynamically")]
+    public Text coinGT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject coinGO = GameObject.Find("CoinCounterText");
+        coinGT = coinGO.GetComponent<Text>();
+        coinGT.text = "Coins Collected: " + collected;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnControllerColliderHit(ControllerColliderHit coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "RedCoin")
+        {
+            Destroy(collidedWith);
+            //int count = int.Parse(coinGT.text);
+            collected += 1;
+            //coinGT.text = count.ToString();
+
+           /* if(count > CoinCounter.CoinsCollected)
+            {
+                CoinCounter.CoinsCollected = count;
+            }*/
+        }
+    }
+}
