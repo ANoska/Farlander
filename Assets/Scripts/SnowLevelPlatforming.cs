04f1d4e531fbad5df3586727abfad100db4d144a
Jake Eckert <jakekert@gmail.com>
2020-12-06 17:56:49 -0500

    snow level platforming challenge started, checkpoint system not working

---
+++ Assets/Scripts/SnowLevelPlatforming.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowLevelPlatforming : MonoBehaviour
+{
+    public GameObject checkpoint1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            Debug.Log("contact");
+            other.gameObject.SetActive(false);
+            other.transform.position = checkpoint1.transform.position;
+            other.gameObject.SetActive(true);
+        }
+    }
+}
