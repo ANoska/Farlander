a95f9ec0ec313456e393f9e69ffc334414a13826
Nathan Midgett <natemidgett@gmail.com>
2020-12-07 13:16:28 -0500

    Level Portal
    Made a portal and script that should take the player to each level. Haven't tested it out yet

---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class LevelChange : MonoBehaviour
+{
+
+    Scene gameScene;
+    string Level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameScene = SceneManager.GetActiveScene();
+        Level = gameScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnControllerColliderHit(ControllerColliderHit coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Player")
+        {
+            if(Level == "ForestLevel")
+            {
+                SceneManager.LoadScene("DesertLevel");
+            }
+            else if(Level == "DesertLevel")
+            {
+                SceneManager.LoadScene("MountainLevel");
+            }
+            else if(Level == "MountainLevel")
+            {
+                SceneManager.LoadScene("SnowLevel");
+            }
+            else if(Level == "SnowLevel")
+            {
+
+            }
+          
+        }
+    }
+}
