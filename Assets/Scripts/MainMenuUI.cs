ce5d5cfa87446eb4e2b64add14e6b43f83c1d904
Austin Noska <noska.austin@gmail.com>
2020-12-07 16:31:22 -0500

    Create Main Menu Scene

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Button playButton;
+
+    private const string FIRST_LEVEL_NAME = "ForestLevel";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(() => SceneManager.LoadScene(FIRST_LEVEL_NAME));
+    }
+}
---
+++ Assets/Scripts/PathFollower.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollower : MonoBehaviour
+{
+
+	public float moveSpeed = 3f;
+	public float rotationSpeed = 3f;
+	public Transform pathParent;
+	Transform targetPoint;
+	int index;
+
+	void OnDrawGizmos()
+	{
+		Vector3 from;
+		Vector3 to;
+		for (int a = 0; a < pathParent.childCount; a++)
+		{
+			from = pathParent.GetChild(a).position;
+			to = pathParent.GetChild((a + 1) % pathParent.childCount).position;
+			Gizmos.color = new Color(1, 0, 0);
+			Gizmos.DrawLine(from, to);
+		}
+	}
+	void Start()
+	{
+		index = 0;
+		targetPoint = pathParent.GetChild(index);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, moveSpeed * Time.deltaTime);
+
+		Vector3 dir = targetPoint.position - transform.position;
+		Quaternion rot = Quaternion.LookRotation(dir);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, rot, rotationSpeed * Time.deltaTime);
+
+		if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f)
+		{
+			index++;
+			index %= pathParent.childCount;
+			targetPoint = pathParent.GetChild(index);
+		}
+	}
+}
